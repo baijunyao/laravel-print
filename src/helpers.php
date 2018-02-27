e3876602b99122aa14b957c7125c9c8ebebc47da
baijunyao <baijunyao@baijunyao.com>
2018-02-27 16:33:50 +0800

    调整字体

--- src/helpers.php
+++ src/helpers.php
@@ -26,17 +26,17 @@ if (!function_exists('p')) {
             }
 
             $dumper->setStyles([
-                'default' => 'background-color:#18171B; color:#FF8400; line-height:1.2em; font:13px Menlo, Monaco, Consolas, monospace; word-wrap: break-word; white-space: pre-wrap; position:relative; z-index:9999; word-break: break-all',
+                'default' => 'background-color:#18171B; color:#FF8400; line-height:1.2em; font-size:14px; font-family:Microsoft Yahei; word-wrap: break-word; white-space: pre-wrap; position:relative; z-index:9999; word-break: break-all',
                 'num' => 'font-weight:bold; color:#1299DA',
                 'const' => 'font-weight:bold',
-                'str' => 'font-weight:bold; color:#56DB3A',
-                'note' => 'color:#1299DA',
+                'str' => 'color:#56DB3A; font-family:Microsoft Yahei;',
+                'note' => 'color:#1299DA; font-family:Microsoft Yahei;',
                 'ref' => 'color:#A0A0A0',
                 'public' => 'color:#FFFFFF',
                 'protected' => 'color:#FFFFFF',
                 'private' => 'color:#FFFFFF',
                 'meta' => 'color:#B729D9',
-                'key' => 'color:#56DB3A',
+                'key' => 'color:#56DB3A; font-family:Microsoft Yahei',
                 'index' => 'color:#1299DA',
                 'ellipsis' => 'color:#FF8400',
             ]);
